67901746d2fc395d6f23df7327b06d1a8dbebf9b
Nan Jia <nan.jia@nzx.com>
2014-04-17 13:03:52 +1000

    more testing added

--- hello.pl
+++ hello.pl
@@ -6,3 +6,8 @@ use warnings;
 my $string = 'Hello World!';
 # display value
 print "$string\n";
+
+my $ms = "this is a multiline string
+because it contains new line char";
+
+my $cs = "String that has # dir, should not be confused as a comment";
