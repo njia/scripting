88c14a6630220534e3eb9e6c7b01b275dde91ca7
Nan Jia <nan.jia@nzx.com>
2014-04-17 16:11:32 +1000

    Mixed number of string printed from 4 to 10

--- PartA.pl
+++ PartA.pl
@@ -80,7 +80,7 @@ sub print_strings {
     s/^([^#]*?)(#.*?$)/$1/ if /#/;
     push @strings, map {$_."\n"} /(".*?"|'.*?')/g
   }
-  my $number = (scalar @strings > 4) ? 4 : (scalar @strings) -1;
+  my $number = (scalar @strings > 10) ? 10 : (scalar @strings) -1;
   print map {$strings[$_]} (0..$number);
   close $IN_FILE;
 }
