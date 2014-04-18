73333c901cd8415480b0dd817aaa1ee60057196a
Nan Jia <nan.jia@nzx.com>
2014-04-19 01:46:31 +1000

    partial working

--- PartD.pl
+++ PartD.pl
@@ -3,18 +3,81 @@
 use warnings;
 use strict;
 use 5.010;
-# use re "debug";
 
-# this sub will download Perl keyword html file from learn.perl.org and create keyword
+my $lines = 0;
+my @words = ();
+my @chars = ();
+
+my @strings = ();
+my @comments = ();
+my @numbers = ();
+
+my %unique_strings;
+my %unique_comments;
+my %unique_keywords;
+
+my $fnumber_regex = qr{([-+]?([0-9_]+(\.[0-9_]+)?|[-+]?\.[0-9_]+)([eE]?[-+]?[0-9_]+)?)\b};
+my $bin_oct_hex = qr{((0[x|X][0-9a-fA-F_]+)|(0[0-7]+?)|(0[b|B][01_]+))};
+my $dquo_re = qr{"(?:(?>[^"\\]+)|\\.)*"};
+my $squo_re = qr{'(?:(?>[^'\\]+)|\\.)*'};
+my $comment_re = qr{(?<!\$)#.*};
+
+my $string_color  = '<code style=color:royalblue>';
+my $keyward_color = '<code style=color:darkred>';
+my $number_color  = '<code style=color:darkcyan>';
+my $comment_color = '<code style=color:darkgreen>';
+my $color_end      = '</code>';
+my $src;
+my $off_set = 0;
+my $end_index = 0;
+my $char;
+
+my $input_file = shift;
+die "Error: unable to analyse the specified file.\n" if !defined $input_file;
+chomp $input_file;
+
+unless ( $input_file =~ /\.p[l|m]$/ && -R -f -s -T $input_file) {
+  die "Error: unable to analyse the specified file.\n";
+  exit 1;
+}
+
+my $output_file = shift;
+die "Error: need output file name\n" if !defined $output_file;
+die "Error: please enter a output file name with .htm or .html.\n" unless $output_file =~ /\.htm|\.html$/;
+
+&read_file;
+
+sub read_file {
+  open my $IN_FILE, "<", $input_file or die "Could not read from $input_file\n";
+  local $/ = undef;
+  $src = <$IN_FILE>;
+  close $IN_FILE;
+}
+
+  &find_keywords($src);
+  foreach my $kw (keys %unique_keywords) {
+    $src =~ s/\b$kw\b/$keyward_color$kw$color_end/g;
+  }
+
+  $src =~ s/($dquo_re)/$string_color$1$color_end/g;
+  $src =~ s/($squo_re)/$string_color$1$color_end/g;
+  $src =~ s/($comment_re)/$string_color$1$color_end/g;
+  $src =~ s/($fnumber_regex|$bin_oct_hex)/$number_color$1$color_end/g;
+
+
+  open my $OUT_FILE, ">", $output_file or die "Could not write to $output_file\n";
+  print $OUT_FILE "<pre>".$src."</pre>";
+  close $OUT_FILE;
+  exit 0;
+
+# This sub will download Perl keyword html file from learn.perl.org and create keyword
 # list for Perl syntac, Perl functions and Perl find handles
 sub get_keywords {
   # Download the Perl keyword HTML file
   my $perl_keywords = `curl -s http://learn.perl.org/docs/keywords.html`;
-  # define start match for Perl file handle
-  # define start match for Perl functions
-  # define start match for Perl syntax
-  # all ends at </table>
-  # every keyword is between ">" and "</a>"
+  # Define start match for Perl filehandles, syntax and functions
+  # All keywords section ends at </table>
+  # And every keyword is between ">" and "</a>"
   my $handle_start   = "File Handles</h3>";
   my $function_start = ">Perl functions</h3>";
   my $syntax_start   = ">Perl syntax</h3>";
@@ -36,127 +99,32 @@ sub get_keywords {
   $perl_keywords =~ /(?:$syntax_start)(.+?)(?:$end_capture)/s;
   $temp = $1;
   my @syntax_keywrod_list = $temp =~ /(?:$key_words_start)([_a-zA-Z\-]+?)(?:$key_words_end)/gs;
-  my %key_words = map { $_ => 1 } (@filehandle_keyword_list, @function_keywoard_list, @syntax_keywrod_list);
+  my %key_words = map { $_ => 1 } (@filehandle_keyword_list, @function_keywoard_list,  @syntax_keywrod_list);
   return %key_words;
 }
 
-sub find_numbers {
-  open my $IN_FILE, "<", $_[0] or die "print_keywords: Could not read from file $_[0]\n";
-  my %numbers;
-  my @words = ();
-  while (<$IN_FILE>) {
-    s/^(#[^!]+$)//;
-    s{(^[^#]+?)(#[^/]+$)}{$1};
-    s/('.*?'|".*?")//g;
-    push @words, split;
-  }
-
-  foreach my $item (@words) {
-    next if $item =~ /^[^-|^+|^\d|^.]/;
-    $numbers{$1}++ if $item =~ s/([-+]?([0-9_]+(\.[0-9_]+)?|[-+]?\.[0-9_]+)([eE]?[-+]?[0-9_]+)?)\b//;
-    $numbers{$1}++ if $item =~ /((0[x|X][0-9a-fA-F_]+)|(0[0-7]+?)|(0[b|B][01_]+))/;
-  }
-  close $IN_FILE;
-  return keys %numbers;
-}
-
 sub find_keywords {
-  open my $IN_FILE, "<", $_[0] or die "print_keywords: Could not read from file $_[0]\n";
   my %perl_key_words = &get_keywords;
   my $number_of_keywords = 0;
-  my %key_words;
-  while (<$IN_FILE>) {
-    chomp;
-    foreach my $word (split) {
+  my $src = shift;
+
+  foreach my $item (@strings) {
+    $src =~ s/$item//g;
+  }
+
+  foreach my $item (@comments) {
+    $src =~ s/$item//g;
+  }
+
+  foreach my $word (split " ", $src) {
+    if ($word =~ /^[-|+|\d|.]/) {
+      # push @numbers, $1 if $word =~ s/([-+]?([0-9_]+(\.[0-9_]+)?|[-+]?\.[0-9_]+)([eE]?[-+]?[0-9_]+)?)\b//;
+      # push @numbers, $1 if $word =~ /((0[x|X][0-9a-fA-F_]+)|(0[0-7]+?)|(0[b|B][01_]+))/;
+    } else {
       $word =~ s/[^@\$%&a-zA-Z_-]//g;
-      $key_words{$word}++ if ($perl_key_words{$word})
+      if ($perl_key_words{$word}) {
+        $unique_keywords{$word}++;
+      }
     }
   }
-  close $IN_FILE;
-  return keys %key_words;
 }
-
-sub find_comments {
-  open my $IN_FILE, "<", $_[0] or die "print_comments: Could not read from file $_[0]\n";
-  my @comments = ();
-  while (<$IN_FILE>) { # inline comments for test
-    push @comments, "$1" if /^(#[^!]+$)/;
-    push @comments, "$1" if m{(?:^[^#]+?)(#[^/]+$)};
-  }
-  close $IN_FILE;
-  return @comments;
-}
-
-sub find_strings {
-  open my $IN_FILE, "<", $_[0] or die "print_strings: Could not read from file $_[0]\n";
-  my @strings = ();
-  local $/ = undef;
-  my $content = <$IN_FILE>;
-  push @strings, $content =~ /(".*?"|'.*?')/sg;
-  close $IN_FILE;
-  return @strings;
-}
-
-sub read_whole_file {
-  open my $IN_FILE, "<", $_[0] or die "print_strings: Could not read from file $_[0]\n";
-  local $/ = undef;
-  my $content = <$IN_FILE>;
-  close $IN_FILE;
-  return $content;
-}
-
-my $string_color  = '<code style="color:#2B60DE">';
-my $keyward_color = '<code style="color:#08B000">';
-my $number_color  = '<code style="color:#008B8B">';
-my $comment_color = '<code style="color:#006400">';
-my $colorend  = '</code>';
-
-my $input_file = $ARGV[0];
-die "Error: unable to analyse the specified file.\n" if !defined $input_file;
-
-my $output_file = $ARGV[1];
-die "Error: Please enter output filename.\n" if !defined $output_file;
-
-unless ( $input_file =~ /.p[l|m]$/ && -R -f -s -T $input_file) {
-  die "Error: unable to analyse the specified file.\n";
-  exit 1;
-}
-
-open my $OUT_FILE, ">", $output_file or die "Could not write to $output_file\n";
-
-my $file_content = &read_whole_file($input_file);
-my @kw = &find_keywords($input_file);
-my @comments = &find_comments($input_file);
-my @strings = &find_strings($input_file);
-
-open my $IN_FILE, "<", $input_file or die "Could not read from $input_file\n";
-
-while (<$IN_FILE>) {
-  if (/^\s*#[^!]/) {
-    s/^/$comment_color/;
-    s/$/$colorend/;
-  }
-}
-
-# part D notes, do numbers first, if line starts with # jj
-
-$file_content =~ s/((0[x|X][0-9a-fA-F_]+)|(0[0-7]+?)|(0[b|B][01_]+))/$number_color$1$colorend/g;
-$file_content =~ s/(?<!["|\w|\[|:#])([-+]?(?!")([0-9_]+(\.[0-9_]+)?|[-+]?\.[0-9_]+)([eE]?[-+]?[0-9_]+)?)(?!\b#\b)/$number_color$1$colorend/g;
-
-# $file_content =~ s/((?<!["|\w|])\d+(?!))/$number_color$1$colorend/g;
-
-# foreach my $item (@kw) {
-#   $file_content =~ s/\b($item)\b/$keyward_color$1$colorend/g;
-# }
-
-  # $file_content =~ s/('.*'|".*")/$string_color$1$colorend/gm;
-  #
-
-  # $file_content =~ s/(^(?:\s*)?(#.*$)|^(?:(\s*)?[^#]+)(#.*$))/$comment_color$1$colorend/g;
-
- # $file_content =~ s/^(?:\s+)(#[^!]+)/$comment_color$1$colorend/;
- # $file_content =~ s{(?:^[^#]+?)(#[^/]+)}{$comment_color$1$colorend};
-
-print $OUT_FILE "<pre>\n".$file_content."\n</pre>";
-close $OUT_FILE;
-exit 0;
