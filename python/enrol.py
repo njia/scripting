b1f197ebf99e75aac2843e87de08a6f0e93787a4
Nan Jia <nan.jia@nzx.com>
2014-05-19 16:29:26 +1000

    Added KeyError, classes finished, started on classinfo

--- python/enrol.py
+++ python/enrol.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 import utility
 import os
+import keyerror
 
 class Enrol:
   def __init__(self, dirname):
+    self.class_list = []
     self.subs = {}
     self.directory = dirname
     self.sub_filename = os.path.join(self.directory, "SUBJECTS")
     self.class_fname  = os.path.join(self.directory, "CLASSES")
     self.allsubs = utility.readlines(self.sub_filename)
     self.allclasses = utility.readlines(self.class_fname)
+
     print self.allclasses
     for line in self.allsubs:
       code, name = line.split(":")
@@ -26,7 +29,23 @@ class Enrol:
     else:
       return
 
-  def classes(self, class_id):
+  def classes(self, sub_code):
+    for line in self.allclasses:
+      if (sub_code in line):
+        self.class_list.append(line.split(":")[0])
+
+    if len(self.class_list) > 0:
+      return self.class_list
+    else:
+      raise KeyError("Not Found")
+
+  def classinfo(self, class_id):
+    class_info = []
     for line in self.allclasses:
       if (class_id in line):
-        print line.split(":")[0]
+        class_info = (line.split(":")[0], line.split(":")[2], line.split(":")[3], line.split(":")[4])
+
+    if len(class_info) > 0:
+      return class_info
+    else:
+      raise KeyError("Class not found")
