3f6c707a5b8073aa874a21f6a522e560386bcf61
Nan Jia <nan.jia@nzx.com>
2014-05-28 18:03:03 +1000

    Part 2 almost done, test-enrol.py still have two errors, also need to check Venuse before enrolling a student

--- python/enrol.py
+++ python/enrol.py
@@ -1,25 +1,54 @@
 #!/usr/bin/python
-import os
-import keyerror
 import glob
-import time
-import shutil
+import keyerror
+import os
 import re
+import shutil
+import time
 
 class Enrol:
   def __init__(self, dirname):
     self.class_list = []
     self.subs = {}
     self.directory = dirname
-    self.sub_filename = os.path.join(self.directory, "SUBJECTS")
-    self.class_fname  = os.path.join(self.directory, "CLASSES")
+    self.sub_filename = os.path.join(dirname, "SUBJECTS")
+    self.class_filename  = os.path.join(dirname, "CLASSES")
+    self.venues_filename = os.path.join(dirname, "VENUES")
+    self.class_stundet_files = glob.glob(os.path.join(dirname, "*.roll"))
     self.allsubs = readlines(self.sub_filename)
-    self.allclasses = readlines(self.class_fname)
-    self.class_stundet_files = glob.glob(os.path.join(self.directory, "*.roll"))
+    self.allclasses = readlines(self.class_filename)
+    self.all_class_venues = readlines(self.venues_filename)
+    self.class_to_room = {}
+    self.classroom_venuse = {}
 
     for line in self.allsubs:
-      code, name = line.split(":")
-      self.subs[code] = name
+      if ":" in line:
+        code, name = line.split(":")
+        self.subs[code] = name
+
+    for line in self.allclasses:
+      if ":" in line:
+        class_name = line.split(":")[0]
+        class_room = line.split(":")[3]
+        self.class_to_room[class_name] = class_room
+
+    for line in self.all_class_venues:
+      if ":" in line:
+        class_room = line.split(":")[0]
+        class_venue = line.split(":")[1]
+        self.classroom_venuse[class_room] = class_venue
+
+  def printClassFileName(self):
+    for filename in self.class_stundet_files:
+      print filename
+
+  def printVenues(self):
+    for key in self.classroom_venuse.keys():
+      print key + " => " + self.classroom_venuse[key]
+
+  def printClassRoom(self):
+    for key in self.class_to_room.keys():
+      print key + " => " + self.class_to_room[key]
 
   def checkStudent(self, student_id, subject_id = ""):
     class_list = []
@@ -30,11 +59,17 @@ class Enrol:
           class_list.append(filename[:-5])
     else:
       for a_class in self.classes(subject_id):
-        if student_id in open('%s' %a_class+".roll").read():
+        filename = os.path.join(self.directory, (a_class + ".roll"))
+        if student_id in readlines(filename):
           a_class = os.path.basename(a_class)
           class_list.append(a_class)
 
-    return class_list
+    if len(class_list) == 1:
+      return class_list[0]
+    elif len(class_list) == 0:
+      return None
+    else:
+      return class_list
 
   def students_of_class(self, class_name):
     filename = os.path.join(self.directory, (class_name + ".roll"))
@@ -76,7 +111,44 @@ class Enrol:
     if (sub_name):
       return (sub_name, class_date, class_room, class_tutor, student_list)
     else:
-      raise KeyError("Class not found")
+      raise KeyError(class_id)
+
+  def subject_of_class(self, class_id):
+    if class_id not in self.class_to_room:
+      raise KeyError
+
+    for a_class in self.allclasses:
+      if class_id in a_class:
+        return a_class.split(":")[1]
+
+  def enrol(self, student_id, class_id):
+    if class_id not in self.class_to_room.keys():
+      raise KeyError
+
+    filename = os.path.join(self.directory, (class_id + ".roll"))
+    a_list = readlines(filename)
+    if student_id in a_list:
+      # print "studnet %s already enrolled in the class %s " %(student_id, class_id)
+      return 1
+
+    subject_class_list = self.classes(self.subject_of_class(class_id))
+    subject_class_list.remove(class_id)
+
+    for a_class in subject_class_list:
+      filename = os.path.join(self.directory, (a_class + ".roll"))
+      student_list_of_a_class = readlines(filename)
+      if student_id in student_list_of_a_class:
+        # print "found student %s in class %s " %(student_id, a_class)
+        student_list_of_a_class.remove(student_id)
+        # type(student_list_of_a_class)
+        writelines(filename, student_list_of_a_class)
+
+    filename = os.path.join(self.directory, (class_id + ".roll"))
+    student_list_of_class = readlines(filename)
+    student_list_of_class.append(student_id)
+    writelines(filename, student_list_of_class)
+
+    return 1
 
 def readlines(filename):
   f = open('%s' % filename, "r")
